b666872d288b301f8d0b3c27c155fb0b7eb2b5f3
Olegs Korsaks <olegs@metisjean.com>
2026-02-09 20:09:08 +0200

    Code improvements

--- src/module/pg/mod.rs
+++ src/module/pg/mod.rs
@@ -1,5 +1,3 @@
 pub fn is_valid_tname(name: &str) -> bool {
-    name.chars().all(|c| {
-        c.is_ascii_alphanumeric() || c == '_'
-    })
+    name.chars().all(|c| c.is_ascii_alphanumeric() || c == '_')
 }
